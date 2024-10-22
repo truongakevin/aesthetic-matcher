a011d4c682d4475c2489be83d2619163cfd5f561
Kevin Truong <42168530+kevinoli@users.noreply.github.com>
2024-10-22 15:20:38 -0600

    sitemap

--- client-astro/astro.config.mjs
+++ client-astro/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from 'astro-sitemap';
 
 export default defineConfig({
+  site: 'https://aestheticmatcher.com',
   integrations: [
     react(),
+    sitemap(),
   ],
-    output: 'static',
+  output: 'static',
 });
